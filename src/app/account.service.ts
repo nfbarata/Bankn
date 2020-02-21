68603c274e26d30d5481b715423b15fbc755d172
nfbarata <nfbarata@hotmail.com>
2020-02-21 23:25:54 +0000

    2 files modified, 3 added

---
+++ src/app/account.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' //TODO para q?
+})
+export class AccountService {
+  accounts[];
+
+  constructor() { }
+
+  ceateAccount(account){
+    this.accounts.push(account);
+  }
+
+  deleteAccount(account){
+    //this.accounts.
+  }
+
+  getAccounts(){
+    return this.accounts;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
+import { AccountService } from './account.service';
+import { AccountCreateComponent } from './account-create/account-create.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, MenuComponent, AccountCreateComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [AccountService]
 })
 export class AppModule { }
