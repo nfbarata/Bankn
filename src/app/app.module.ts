bb3470d86c99150f8ce280c90e0128cb2e5fbd6a
nfbarata <nfbarata@hotmail.com>
2020-02-21 23:27:09 +0000

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
 import { AccountService } from './account.service';
 import { AccountCreateComponent } from './account-create/account-create.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent, AccountCreateComponent ],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, AccountCreateComponent, AccountListComponent, AccountDetailsComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AccountService]
 })
