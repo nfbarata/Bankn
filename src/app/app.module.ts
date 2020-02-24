4c2c936f94d2d43fcc0aa8db39729151b2d1ba97
nfbarata <nfbarata@hotmail.com>
2020-02-24 19:31:44 +0000

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { APP_BASE_HREF } from '@angular/common';//from erro
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -11,6 +13,8 @@ import { HomeComponent } from './shared/components/home/home.component';
 
 import { AccountService } from './shared/services/account.service';
 import { FileService } from './shared/services/file.service';
+
+registerLocaleData(localeFr, 'fr-FR');
 
 @NgModule({
   imports:      [ 
