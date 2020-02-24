adee84256972fa7756ca2038ccb7f03ada335b79
nfbarata <nfbarata@hotmail.com>
2020-02-24 16:36:41 +0000

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   //{ path: '', pathMatch: 'full', redirectTo: 'content' },
   { path: '', pathMatch: 'full', component: HomeComponent },
   //{ path: 'accounts', component: AccountModule},
-  //{ path: 'accounts', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule)},
+  { path: 'accounts', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule)},
 /*  {
     path: 'accounts',
     loadChildren: './modules/account/account.module#AccountModule',
