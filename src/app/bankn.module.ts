8377038268751dd18307f92c0a426cd7a6ff3095
nfbarata <nfbarata@hotmail.com>
2020-02-25 18:27:01 +0000

    1 file modified, 1 added, 1 deleted

--- src/app/bankn.module.ts
+++ src/app/bankn.module.ts
@@ -2,35 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { APP_BASE_HREF } from '@angular/common';//from erro
+
 import { registerLocaleData } from '@angular/common';
 import localeEn from '@angular/common/locales/en';
 import localePt from '@angular/common/locales/pt';
+registerLocaleData(localeEn, 'en-EN');
+registerLocaleData(localePt, 'pt-PT');
 
-import { AppRoutingModule } from './app-routing.module';
+import { BanknRoutingModule } from './bankn-routing.module';
 
 import { AccountService } from './shared/services/account.service';
 import { FileService } from './shared/services/file.service';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './shared/components/menu/menu.component';
-import { MenuSideComponent } from './shared/components/menu-side/menu-side.component';
-import { HomeComponent } from './shared/components/home/home.component';
-
-registerLocaleData(localeEn, 'en-EN');
-registerLocaleData(localePt, 'pt-PT');
+import { BanknComponent } from './components/bankn.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuSideComponent } from './components/menu-side/menu-side.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule, ReactiveFormsModule,
-    AppRoutingModule
+    BanknRoutingModule
   ],
   exports: [
   ],
   declarations: [ 
-    AppComponent, MenuComponent, HomeComponent, MenuSideComponent
+    BanknComponent, MenuComponent, HomeComponent, MenuSideComponent
   ],
-  bootstrap: [ AppComponent ],
+  bootstrap: [ BanknComponent ],
   providers: [
     {provide: APP_BASE_HREF, useValue : '/' }, 
     AccountService, FileService
