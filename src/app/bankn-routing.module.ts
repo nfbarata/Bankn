fbe918868fcb5330687a309b7e5ba06da9caf01d
nfbarata <nfbarata@hotmail.com>
2020-02-25 18:27:32 +0000

    1 file modified

--- src/app/bankn-routing.module.ts
+++ src/app/bankn-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './shared/components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   //{ path: '', pathMatch: 'full', redirectTo: 'content' },
