b43702b6a399de1656c09e4fee5023f78bb2a95c
nfbarata <nfbarata@hotmail.com>
2020-02-25 18:19:53 +0000

    1 file modified

--- src/app/modules/transaction/transaction.module.ts
+++ src/app/modules/transaction/transaction.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
-//import {AppModule} from '../../app.module'
-
 import { TransactionListComponent } from './transaction-list/transaction-list.component'
 
 @NgModule({
