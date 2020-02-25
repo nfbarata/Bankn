e9fcc52dad6622809bec858e695c0adb79ce012e
nfbarata <nfbarata@hotmail.com>
2020-02-25 17:36:58 +0000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { APP_BASE_HREF } from '@angular/common';//from erro
 import { registerLocaleData } from '@angular/common';
+import localeEn from '@angular/common/locales/en';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { AccountService } from './shared/services/account.service';
+import { FileService } from './shared/services/file.service';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/components/menu/menu.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { AccountSelectComponent } from './shared/components/account-select/account-select.component';
 import { AccountCreateComponent } from './shared/components/account-create/account-create.component';
-
-
-import { AccountService } from './shared/services/account.service';
-import { FileService } from './shared/services/file.service';
-
-import localeEn from '@angular/common/locales/en';
-import localeFr from '@angular/common/locales/fr';
-import localePt from '@angular/common/locales/pt';
 import { MenuSideComponent } from './shared/components/menu-side/menu-side.component';
-import { AccountCreateComponent } from './shared/components/account-create/account-create.component';
 
 registerLocaleData(localeEn, 'en-EN');
 registerLocaleData(localePt, 'pt-PT');
@@ -32,7 +28,7 @@ registerLocaleData(localePt, 'pt-PT');
     AppRoutingModule
   ],
   declarations: [ 
-    AppComponent, MenuComponent, HomeComponent, MenuSideComponent, AccountCreateComponent,
+    AppComponent, MenuComponent, HomeComponent, MenuSideComponent, AccountCreateComponent, AccountSelectComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [
