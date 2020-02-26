8298cf0eb69435ce0efce420e9cc00d82f02b87f
nfbarata <nfbarata@hotmail.com>
2020-02-26 16:09:21 +0000

    2 files modified

--- src/modules/account/account-create/account-create.component.ts
+++ src/modules/account/account-create/account-create.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 import { AccountService } from '../../../services/account.service';
 import  { Account } from "../../../models/account";
-import { Dinero } from 'dinero.js';
 
 @Component({
   selector: 'account-create',
