28c410d567bd2470a5b631375acc5d0bca3240de
nfbarata <nfbarata@hotmail.com>
2020-02-26 12:01:52 +0000

    2 files modified

--- src/modules/account/account-create/account-create.component.ts
+++ src/modules/account/account-create/account-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder  } from '@angular/forms';
+import { FormBuilder, FormGroup  } from '@angular/forms';
 import { AccountService } from '../../../services/account.service';
 
 @Component({
