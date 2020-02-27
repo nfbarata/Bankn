e28e48f3d7a5709d3d978632080c65f56b4dc4aa
nfbarata <nfbarata@hotmail.com>
2020-02-27 19:48:58 +0000

    1 file modified

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AccountSelectCardComponent } from './account-select-card/account-select-card.component';
 import { AccountCreateCardComponent } from './account-create-card/account-create-card.component';
@@ -7,7 +8,7 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [
     AccountCreateCardComponent, AccountSelectCardComponent, FileUploadComponent
