5924d476eac2a42b21a74c14eed0d8129499f852
nfbarata <nfbarata@hotmail.com>
2020-02-27 21:13:59 +0000

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './polyfills';
 
 import { enableProdMode } from '@angular/core';
-//enableProdMode();
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './modules/app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   
