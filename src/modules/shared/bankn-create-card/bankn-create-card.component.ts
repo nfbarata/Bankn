648d5eca6446d075fe9a92ba84ba3c547f46a47b
nfbarata <nfbarata@hotmail.com>
2020-02-28 14:13:49 +0000

    1 file modified, 3 added

---
+++ src/modules/shared/bankn-create-card/bankn-create-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bankn-create-card',
+  templateUrl: './bankn-create-card.component.html',
+  styleUrls: ['./bankn-create-card.component.css']
+})
+export class BanknCreateCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { AccountSelectCardComponent } from './account-select-card/account-select
 import { AccountCreateCardComponent } from './account-create-card/account-create-card.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileOpenCardComponent } from './file-open-card/file-open-card.component';
+import { BanknCreateCardComponent } from './bankn-create-card/bankn-create-card.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,7 @@ import { FileOpenCardComponent } from './file-open-card/file-open-card.component
     AccountCreateCardComponent, AccountSelectCardComponent, FileUploadComponent, FileOpenCardComponent
   ],
   declarations: [
-    AccountCreateCardComponent, AccountSelectCardComponent, FileUploadComponent, FileOpenCardComponent
+    AccountCreateCardComponent, AccountSelectCardComponent, FileUploadComponent, FileOpenCardComponent, BanknCreateCardComponent
   ]
 })
 export class SharedModule { }
