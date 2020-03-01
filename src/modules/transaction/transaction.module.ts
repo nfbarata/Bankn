98d1b1b72fb42c04e13b1acb5237d4e7839e0b4c
nfbarata <nfbarata@hotmail.com>
2020-03-01 17:07:16 +0000

    1 file modified

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
