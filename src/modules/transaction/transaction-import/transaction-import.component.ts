cb9240f61e911e089caaef5a1ce597b73584f3ca
nfbarata <nfbarata@hotmail.com>
2020-03-03 21:49:59 +0000

    2 files modified

--- src/modules/transaction/transaction-import/transaction-import.component.ts
+++ src/modules/transaction/transaction-import/transaction-import.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Location} from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
+import { ActivatedRoute,Router } from '@angular/router';
+import { EventsService } from '../../../services/events.service';
+import { BanknService } from '../../../services/bankn.service';
+import { AccountService } from '../../../services/account.service';
+import { TransactionService } from '../../../services/transaction.service';
+import { Account } from "../../../models/account";
+import { Transaction, TransactionType, getTransactionType } from "../../../models/transaction";
 
 @Component({
   selector: 'transaction-import',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionImportComponent implements OnInit {
 
-  constructor() { }
+  form;
+  formData;
+
+  constructor(
+    private eventsService: EventsService,
+    private banknService: BanknService,
+    private accountService: AccountService,
+    private transactionService: TransactionService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location
+  ) { 
+    this.formData = {
+      description:null
+    }
+    this.form = this.formBuilder.group(this.formData);
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      var accountId = params.get('accountId');
+    });
+  }
+
+  onSubmit(data) {
+    this.form.reset();
+    this.router.navigate(['/transactions']);
   }
 
 }
