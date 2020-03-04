08a339b552e8e5906fc7574650978eb066c62924
nfbarata <nfbarata@hotmail.com>
2020-03-04 10:33:23 +0000

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { InitializedGuard } from './guards/initialized.guard';
 import { HomeComponent } from './components/home/home.component';
 import { BanknCreateComponent } from './components/bankn-create/bankn-create.component';
 
@@ -8,9 +9,9 @@ const routes: Routes = [
   //{ path: '', pathMatch: 'full', redirectTo: 'content' },
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'bankn', component: BanknCreateComponent },
-  { path: 'bankn/:banknId', component: BanknCreateComponent },
-  { path: 'accounts', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule)},
-  { path: 'transactions', loadChildren: () => import('./modules/transaction/transaction.module').then(m => m.TransactionModule)},
+  { path: 'bankn/:banknId', component: BanknCreateComponent, canActivate: [InitializedGuard] },
+  { path: 'accounts', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule), canActivate: [InitializedGuard]},
+  { path: 'transactions', loadChildren: () => import('./modules/transaction/transaction.module').then(m => m.TransactionModule), canActivate: [InitializedGuard]},
 ]
 
 @NgModule({
--- src/app/guards/initialized.guard.ts
+++ src/app/guards/initialized.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { BanknService } from '../services/bankn.service';
@@ -8,6 +8,7 @@ import { BanknService } from '../services/bankn.service';
 export class InitializedGuard implements CanActivate {
 
   constructor(
+    private router: Router,
     private banknService:BanknService
   ){
   } 
@@ -15,6 +16,11 @@ export class InitializedGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.banknService.initialized();
+    if(this.banknService.initialized()){
+      return true;
+    } else {
+      this.router.navigate(['']);
+      return false;
+    }
   }
 }
