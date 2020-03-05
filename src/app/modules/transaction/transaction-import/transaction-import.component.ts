4282b5fe71c0727e35fa3a511e119782a55c2c33
nfbarata <nfbarata@hotmail.com>
2020-03-05 13:50:00 +0000

    5 files modified

--- src/app/modules/transaction/transaction-import/transaction-import.component.ts
+++ src/app/modules/transaction/transaction-import/transaction-import.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, Directive } from '@angular/core';
 import { Location} from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 import { ActivatedRoute,Router } from '@angular/router';
@@ -8,13 +8,27 @@ import { AccountService } from '../../../services/account.service';
 import { TransactionService } from '../../../services/transaction.service';
 import { Account } from "../../../models/account";
 import { Transaction, TransactionType, getTransactionType } from "../../../models/transaction";
+import { HighlightTag } from 'angular-text-input-highlight';
+
+/*@Directive({selector: 'importData'})
+export class TextArea {
+  
+}*/
 
 @Component({
   selector: 'transaction-import',
   templateUrl: './transaction-import.component.html',
   styleUrls: ['./transaction-import.component.css']
 })
-export class TransactionImportComponent implements OnInit {
+export class TransactionImportComponent implements OnInit, AfterViewInit {
+  text = 'this is some text';
+  tags: HighlightTag[] = [{
+    indices: { start: 8, end: 12 },
+    cssClass: 'bg-blue',
+    data: { user: { id: 1 } }
+  }];
+  //@ViewChild(TextArea) importData;
+  //@ViewChild('importData',{static:false}) importData2:ElementRef;
 
   form;
   formData;
@@ -30,17 +44,29 @@ export class TransactionImportComponent implements OnInit {
     private location: Location
   ) { 
     this.formData = {
-      description:null,
+      importData:null,
       columnSeparator:null,
       lineSeparator:null
     }
     this.form = this.formBuilder.group(this.formData);
   }
 
+  ngAfterViewInit(): void {
+    /*console.log(this.importData);
+    console.log(this.importData2);
+    this.importData2.nativeElement.highlightWithinTextarea({
+      highlight: "aa" // string, regexp, array, function, or custom object
+    });*/
+  }
+
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       var accountId = params.get('accountId');
     });
+/*
+    $('description').highlightWithinTextarea({
+      highlight: "aa" // string, regexp, array, function, or custom object
+    });*/
   }
 
   onSubmit(data) {
