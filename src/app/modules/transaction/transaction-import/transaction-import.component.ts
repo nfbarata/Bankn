a6e8eb962d3809bc43a7f9c7165befeadbcc089a
nfbarata <nfbarata@hotmail.com>
2020-03-06 18:18:49 +0000

    1 file modified

--- src/app/modules/transaction/transaction-import/transaction-import.component.ts
+++ src/app/modules/transaction/transaction-import/transaction-import.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, Directive, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, Directive, ViewEncapsulation, Renderer2 } from '@angular/core';
 import { Location} from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 import { ActivatedRoute,Router } from '@angular/router';
@@ -16,18 +16,19 @@ import { Transaction, TransactionType, getTransactionType } from "../../../model
  // encapsulation: ViewEncapsulation.None
 })
 export class TransactionImportComponent implements OnInit, AfterViewInit {
-  
+  submitDisabled: boolean=true;
   text = '';
-
   @ViewChild('importData',{static:false}) importData:ElementRef;
   @ViewChild('columnSeparator',{static:false}) columnSeparator:ElementRef;
   @ViewChild('lineSeparator',{static:false}) lineSeparator:ElementRef;
   @ViewChild('parsedData',{static:false}) parsedData:ElementRef;
+  @ViewChild('submitHelpBlock',{static:false}) submitHelpBlock:ElementRef;
 
   form;
   formData;
 
   constructor(
+    private renderer: Renderer2,
     private eventsService: EventsService,
     private banknService: BanknService,
     private accountService: AccountService,
@@ -39,8 +40,8 @@ export class TransactionImportComponent implements OnInit, AfterViewInit {
   ) { 
     this.formData = {
       importData:null,
-      columnSeparator:" ",
-      lineSeparator:"n"
+      columnSeparator:"",
+      lineSeparator:"&#10;"
     }
     this.form = this.formBuilder.group(this.formData);
   }
@@ -56,20 +57,36 @@ export class TransactionImportComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit(data) {
- 
+
     this.form.reset();
     this.router.navigate(['/transactions']);
   }
 
   onInputChange(){
     var data = this.importData.nativeElement.value;
-    console.log(this.lineSeparator.nativeElement.value);
-    console.log(this.columnSeparator.nativeElement.value);
     var lines = data.split(this.lineSeparator.nativeElement.value);
-
-    console.log(lines);
+    if(lines.length>0 && lines[0].trim().length>0){
+      var columns = lines[0].split(this.columnSeparator.nativeElement.value);
+      console.log(columns);
+      if(columns.length>3){
+        this.renderer.setProperty(this.submitHelpBlock.nativeElement, 'innerHTML', '');
+        this.submitDisabled = false;
+      }else{
+        this.renderer.setProperty(this.submitHelpBlock.nativeElement, 'innerHTML', 'There should be at least 3 columns');
+        this.submitDisabled = true;
+      }
+    }else{
+      this.renderer.setProperty(this.submitHelpBlock.nativeElement, 'innerHTML', 'Enter some text');
+      this.submitDisabled = true;
+    }
     //this.parsedData
+/*
+const h1 = this.renderer.createElement('h1');
+    const text = this.renderer.createText('Hello world');
 
+    this.renderer.appendChild(h1, text);
+    this.renderer.appendChild(this.el.nativeElement, div);
+*/
 
   }
 }
