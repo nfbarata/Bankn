98776a02b230dbbedc14a89015233f066b521175
nfbarata <nfbarata@hotmail.com>
2020-03-10 17:00:29 +0000

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
-//  production: true,
-  production: false,
+  production: true,
+/*  production: false,
   bankn : {
     id:'1',
     name:'teste',
@@ -66,5 +66,5 @@ export const environment = {
         ]
       },
     ],
-  }
+  }*/
 };
