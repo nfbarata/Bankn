ee5ba1530b2d770dd849ea6425bd15733a32ad8e
nfbarata <nfbarata@hotmail.com>
2020-03-10 16:22:33 +0000

    2 files modified

--- src/app/modules/transaction/transaction-import-edit/transaction-import-edit.component.ts
+++ src/app/modules/transaction/transaction-import-edit/transaction-import-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, Directive, ViewEncapsulation, Renderer2, Inject } from '@angular/core';
+import { Location, DOCUMENT } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
+import { ActivatedRoute,Router } from '@angular/router';
+import { EventsService } from '../../../services/events.service';
+import { BanknService } from '../../../services/bankn.service';
+import { AccountService } from '../../../services/account.service';
+import { TransactionService } from '../../../services/transaction.service';
+import { Account } from "../../../models/account";
+import { Transaction, TransactionType, getTransactionType, ImportColumnType, getImportColumnType } from "../../../models/transaction";
 
 @Component({
   selector: 'app-transaction-import-edit',
@@ -7,9 +16,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionImportEditComponent implements OnInit {
 
-  constructor() { }
+  form;
+  formData;
+  account;
+  transactions;
+  document;
+
+  constructor(
+    private renderer: Renderer2,
+    private eventsService: EventsService,
+    private banknService: BanknService,
+    private accountService: AccountService,
+    private transactionService: TransactionService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    @Inject(DOCUMENT) document
+  ) {
+    this.document = document;
+    this.formData = {
+      importData:null,
+    }
+    this.form = this.formBuilder.group(this.formData);
+   }
 
   ngOnInit() {
+    this.account=null;
+    this.transactions = this.transactionService.filterTransactions;
+    this.route.paramMap.subscribe(params => {
+      var accountId = params.get('accountId');
+      this.account = this.accountService.getAccount(accountId);
+      if(this.account==null){
+        this.router.navigate(['']);
+      }
+      if(this.transactions.length==0){
+        this.router.navigate(['/transactions/import/'+this.account.Id]);
+      }
+    });
   }
 
+  onSubmit(data) {
+    this.form.reset();
+    this.accountService.selectAccount(this.account);
+    this.router.navigate(['/transactions/'+this.account.id]);
+  }
 }
