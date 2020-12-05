b895660314f6e47f39d4b7a3dfbec314c651ae27
nfbarata <nfbarata@hotmail.com>
2020-12-05 14:03:22 +0000

    2 files modified

--- src/app/modules/account/account/account.component.ts
+++ src/app/modules/account/account/account.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Location} from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
-import { ActivatedRoute,Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { BanknService } from '../../../services/bankn.service';
-import { AccountService } from '../../../services/account.service';
+import { BanknService } from "../../../services/bankn.service";
+import { AccountService } from "../../../services/account.service";
 import { Account } from "../../../models/account";
 
 @Component({
-  selector: 'account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  selector: "account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.css"]
 })
 export class AccountComponent implements OnInit {
-
-  form;
+  form: FormGroup;
   formData;
   countries;
-  
+
   constructor(
     private banknService: BanknService,
     private accountService: AccountService,
@@ -25,87 +24,92 @@ export class AccountComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private location: Location
-    ) { 
-      this.formData = {
-        id:null,
-        name:null,
-        referenceAmount:null,
-        referenceCountry:null,
-        referenceDay:null,
-        referenceMonth:null,
-        referenceYear:null,
-        description:null
-      }
-      this.form = this.formBuilder.group(this.formData);
+  ) {
+    this.formData = {
+      id: null,
+      name: null,
+      referenceAmount: null,
+      referenceCountry: null,
+      referenceDay: null,
+      referenceMonth: null,
+      referenceYear: null,
+      description: null
+    };
+    this.form = this.formBuilder.group(this.formData);
   }
 
   ngOnInit() {
     this.countries = this.banknService.getCountries();
     this.route.paramMap.subscribe(params => {
-      var accountId:string = params.get('accountId');
+      var accountId: string = params.get("accountId");
 
-      if(accountId==null || accountId.trim().length==0){
+      if (accountId == null || accountId.trim().length == 0) {
         this.formData = {
-          id:null,
-          name:'Main',
-          referenceAmount:0,
-          referenceCountry:this.banknService.getReferenceCountry(),
-          referenceDay:'1',
-          referenceMonth:'1',
-          referenceYear:'2000',
-          description:''
-        }
-        this.form.setValue(this.formData);
-        
-      }else{
-        var account:Account = this.accountService.getAccount(accountId);
-
-        this.formData = {
-          id:account.getId(),
-          name:account.name,
-          referenceAmount:account.referenceAmount.toUnit(),
-          referenceCountry:account.referenceCountry,
-          referenceDay:account.referenceDate.getDate(),
-          referenceMonth:account.referenceDate.getMonth()+1,
-          referenceYear:account.referenceDate.getFullYear(),
-          description:account.description
+          id: null,
+          name: "Main",
+          referenceAmount: 0,
+          referenceCountry: this.banknService.getReferenceCountry(),
+          referenceDay: "1",
+          referenceMonth: "1",
+          referenceYear: "2000",
+          description: ""
         };
         this.form.setValue(this.formData);
-      } 
+      } else {
+        var account: Account = this.accountService.getAccount(accountId);
+
+        this.formData = {
+          id: account.getId(),
+          name: account.name,
+          referenceAmount: account.referenceAmount.toUnit(),
+          referenceCountry: account.referenceCountry,
+          referenceDay: account.referenceDate.getDate(),
+          referenceMonth: account.referenceDate.getMonth() + 1,
+          referenceYear: account.referenceDate.getFullYear(),
+          description: account.description
+        };
+        this.form.setValue(this.formData);
+      }
     });
   }
 
   onSubmit(data) {
-    var currency = this.banknService.getCurrencyOfCountry(data.referenceCountry);
-    
-    var amount = this.accountService.toDinero(currency,data.referenceAmount);
+    var currency = this.banknService.getCurrencyOfCountry(
+      data.referenceCountry
+    );
 
-    var date = new Date(0);//clear hours/minutes/seconds
-    date.setFullYear(data.referenceYear, data.referenceMonth-1, data.referenceDay);
-    
-    if(data.id==null){
+    var amount = this.accountService.toDinero(currency, data.referenceAmount);
+
+    var date = new Date(0); //clear hours/minutes/seconds
+    date.setFullYear(
+      data.referenceYear,
+      data.referenceMonth - 1,
+      data.referenceDay
+    );
+
+    if (data.id == null) {
       this.accountService.createAccount(
         data.name,
         data.description,
-        amount,//.toObject(),
+        amount, //.toObject(),
         date,
         data.referenceCountry
       );
-    }else{
+    } else {
       this.accountService.updateAccount(
         data.id,
         data.name,
         data.description,
-        amount,//.toObject(),
+        amount, //.toObject(),
         date,
         data.referenceCountry
       );
     }
     this.form.reset();
-    this.router.navigate(['/accounts']);
+    this.router.navigate([""]);
   }
 
-  onDelete(accountId:string){
+  onDelete(accountId: string) {
     this.accountService.deleteAccountId(accountId);
     this.location.back();
   }
